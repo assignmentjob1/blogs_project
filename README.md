0a50414e08a106e2cf115f2b9ec8ae455050f298
SYSTEMS\skumail.ali <skumail.ali@systemsltd.com>
2021-12-13 22:12:06 +0500

    Blogs Application

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # blogs_project
 Blogs Application
+
+Angular CLI: 12.2.4
+Node: 14.17.6
+Package Manager: npm 6.14.15
+OS: win32 x64
+Springboot: 2.6.1
---
+++ blogs-crud-app/src/main/java/com/example/blogs/BlogsCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsCrudAppApplication.class, args);
+	}
+
+}
